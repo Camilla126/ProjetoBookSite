cb368e69d8aada73a6acde016624a994f98b9056
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-01-06 13:53:59 -0300

    Rotas configuradas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import { BrowserRouter } from "react-router-dom";
+import MainRoutes from "./routes";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <MainRoutes />
+  </BrowserRouter>
+);
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>APPPP</h1>
+    </div>
+  );
+}
+
+export default Home;
