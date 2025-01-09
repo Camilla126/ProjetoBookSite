cf7462ce7bf25e8cdec3483230e251c1b5950129
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-01-09 11:52:02 -0300

    Fazendo estrutura da págian de Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,11 +3,24 @@ import styles from "./styles.module.scss";
 const Login = () => {
   return (
     <main className={styles.main}>
-      <section>
-        <form>
-          <h1>Login</h1>
-        </form>
-      </section>
+      <img
+        src="../../../src/assets/IMG_logincadastro/Intersect.png"
+        alt="Book"
+        width="200px"
+      />
+      <form>
+        <h3>BookWorms</h3>
+        <h2>Faça Login</h2>
+        <label>Email </label>
+        <input type="email" placeholder="Digite um @email" name="email" />
+        <label htmlFor="">Senha</label>
+        <input type="password" placeholder="Digite uma senha" name="password" />
+        <input type="checkbox" /> Lembrar senha
+        <button>Login</button>
+        <p>
+          Não tem uma conta? <a href="#">Cadastre-se</a>
+        </p>
+      </form>
     </main>
   );
 };
