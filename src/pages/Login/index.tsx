235529107e387377a622176b495879350563d6b2
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-01-09 13:42:08 -0300

    aplicando funcionalidades

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,25 +1,37 @@
+import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
+
+import { auth } from "../../firebaseConnection";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   return (
+    const [email, setEmail] = useState('')
+    const [password, setPassword ] = useState('')
+    
+    const navigate = useNavigate();
+
     <main className={styles.main}>
       <img
         src="../../../src/assets/IMG_logincadastro/Intersect.png"
         alt="Book"
-        width="200px"
       />
       <form>
         <h3>BookWorms</h3>
         <h2>Faça Login</h2>
-        <label>Email </label>
-        <input type="email" placeholder="Digite um @email" name="email" />
-        <label htmlFor="">Senha</label>
-        <input type="password" placeholder="Digite uma senha" name="password" />
+        <div>
+          <label>Email </label>
+          <input type="email" placeholder="Digite um @email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="">Senha</label>
+          <input type="password" placeholder="********" name="password" />
+        </div>
         <input type="checkbox" /> Lembrar senha
-        <button>Login</button>
-        <p>
-          Não tem uma conta? <a href="#">Cadastre-se</a>
-        </p>
+        <button type="submit">Login</button>
+        <Link to={"/register"}>Não tem uma conta? Cadastre-se</Link>
       </form>
     </main>
   );
