f1444b6fbc6fdff0a9c0ed3c60faa22224cbc348
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-01-23 12:24:47 -0300

    Estilização do header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -7,33 +7,48 @@ import { IoHomeOutline } from "react-icons/io5";
 import { GoBookmark } from "react-icons/go";
 import { useContext } from "react";
 import { AuthContext, AuthContextInterface } from "../../contexts/auth";
+
 export default function Header() {
   const { logOut, user } = useContext(AuthContext) as AuthContextInterface;
   const navigate = useNavigate();
+
   return (
     <header className={styles.main}>
-      <div>
-        <FaUserCircle />
-        {user ? (
-          <span className={styles.userName}>{user.name}</span>
-        ) : (
-          <span className={styles.userName}></span>
-        )}
-        <div className={styles.navegation}>
-          <IoHomeOutline /> <Link to="/home">Home</Link> <CgFeed />{" "}
-          <Link to="/feed">Feed</Link> <GoBookmark />
-          <Link to="/saved">Saved</Link>
-        </div>
-        <IoIosLogOut />
-        <button
+      {/* Seção do usuário */}
+      <div className={styles.userSection}>
+        <FaUserCircle className={styles.iconUser} />
+        <span className={styles.userName}>{user?.name || "Usuário"}</span>
+      </div>
+
+      {/* Navegação */}
+      <nav className={styles.navegation}>
+        <Link to="/home" className={styles.navItem}>
+          <IoHomeOutline className={styles.icon} />
+          Home
+        </Link>
+        <Link to="/feed" className={styles.navItem}>
+          <CgFeed className={styles.icon} />
+          Feed
+        </Link>
+        <Link to="/saved" className={styles.navItem}>
+          <GoBookmark className={styles.icon} />
+          Saved
+        </Link>
+      </nav>
+
+      {/* Botão de sair */}
+      <div className={styles.logoutSection}>
+        <Link
+          to="/"
+          className={styles.logoutLink}
           onClick={() => {
             logOut();
             navigate("/");
           }}
         >
-          Sai
-        </button>
-        r
+          <IoIosLogOut className={styles.iconSair} />
+          Sair
+        </Link>
       </div>
     </header>
   );
