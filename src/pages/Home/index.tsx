93ee9c2f225b75ec3d9425a4c9782a54b9bf20db
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-01-24 13:10:51 -0300

    Estilizando home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,34 @@
-const Home = () => {
-  return (
-    <div>
-      <h1>HOME</h1>
-    </div>
-  );
-};
+import { GiSpellBook } from "react-icons/gi";
+import { LuNotebookPen } from "react-icons/lu";
+import { RiBookShelfLine } from "react-icons/ri";
+import { PiRankingLight } from "react-icons/pi";
+import IntersectImage from "../../assets/IMG_home/Group 1.png";
 
-export default Home;
+import styles from "./styles.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>
+        Bem vindo ao BookWorms <GiSpellBook className={styles.iconTitle} />
+      </h1>
+
+      <div className={styles.buttonContainer}>
+        <button>
+          <p>Criar minha história</p>
+          <LuNotebookPen className={styles.icons} />
+        </button>
+        <button>
+          <p>Meus livros</p>
+          <RiBookShelfLine className={styles.icons} />
+        </button>
+        <button>
+          <p>Melhores leitores</p>
+          <PiRankingLight className={styles.icons} />
+        </button>
+      </div>
+
+      <img src={IntersectImage} alt="Book" />
+    </main>
+  );
+}
