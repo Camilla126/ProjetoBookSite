aca9b082ee441759db22f34ad4941a4bb2b8fd26
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-03-21 11:37:27 -0300

    Estilizando página Mystory

--- src/pages/MyStory/index.tsx
+++ src/pages/MyStory/index.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react";
 import { StoryContext } from "../../contexts/StoryContext";
 import { toast } from "react-toastify";
+import styles from "../Feed/styles.module.scss";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const MyStory = () => {
   const {
@@ -25,63 +27,67 @@ const MyStory = () => {
   };
 
   return (
-    <div>
-      <h1>Minhas histórias</h1>
-      {loadingStories ? (
-        <p>Carregando histórias...</p>
-      ) : (
-        <div>
-          {myStories.length > 0 ? (
-            myStories.map((story) => (
-              <div key={story.id}>
-                <p>
-                  <strong>Criado em:</strong>
-                  {story.createdAt instanceof Date
-                    ? story.createdAt.toLocaleDateString("pt-BR", {
-                        day: "2-digit",
-                        month: "2-digit",
-                        year: "numeric",
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      })
-                    : "Data desconhecida"}
-                </p>
-                <h2>{story.title}</h2>
-                <p>{story.content}</p>
-                <button onClick={() => setStoryToDelete(story)}>Excluir</button>
+    <main className={styles.feed}>
+      <div className={styles.feedContent}>
+        <h1>Minhas histórias</h1>
+        {loadingStories ? (
+          <div className={styles.loadingContainer}>
+            <div className={styles.spinner}></div>
+            <p>Carregando histórias...</p>
+          </div>
+        ) : (
+          <div className={styles.feedStory}>
+            {myStories.length > 0 ? (
+              myStories.map((story) => (
+                <div key={story.id} className={styles.feedStoryContent}>
+                  <p className={styles.DatePublish}>
+                    <strong>Criado em:</strong>
+                    {story.createdAt instanceof Date
+                      ? story.createdAt.toLocaleDateString("pt-BR", {
+                          day: "2-digit",
+                          month: "2-digit",
+                          year: "numeric",
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })
+                      : "Data desconhecida"}
+                  </p>
+                  <h2>{story.title}</h2>
+                  <p className={styles.contentCreateStory}>{story.content}</p>
+                  <RiDeleteBin6Line
+                    className={styles.iconDelete}
+                    onClick={() => setStoryToDelete(story)}
+                  />
 
-                {!story.published && (
-                  <button onClick={() => handlePublish(story.id!)}>
-                    Publicar
-                  </button>
-                )}
+                  {!story.published && (
+                    <button onClick={() => handlePublish(story.id!)}>
+                      Publicar
+                    </button>
+                  )}
+                </div>
+              ))
+            ) : (
+              <p>Nenhuma história criada no momento.</p>
+            )}
+          </div>
+        )}
+
+        {storyToDelete && (
+          <div className={styles.modal}>
+            <div className={styles.modalContent}>
+              <p>
+                Tem certeza que deseja excluir permanentemente"
+                {storyToDelete.title}"?
+              </p>
+              <div className={styles.buttons}>
+                <button onClick={() => deleteStory(false)}>Sim</button>
+                <button onClick={() => setStoryToDelete(null)}>Não</button>
               </div>
-            ))
-          ) : (
-            <p>Nenhuma história criada no momento.</p>
-          )}
-        </div>
-      )}
-
-      {storyToDelete && (
-        <div className="modal">
-          <div className="modal-content">
-            <p>
-              Tem certeza que deseja excluir permanentemente? "
-              {storyToDelete.title}"?
-            </p>
-            <div className="buttons">
-              <button onClick={() => deleteStory(false)} className="confirm">
-                Sim
-              </button>
-              <button onClick={() => setStoryToDelete(null)} className="cancel">
-                Não
-              </button>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </main>
   );
 };
 
